f477de8ef518b7c11507c04496f91685863d29da
kiburei <akiburei@gmail.com>
2018-01-09 15:51:46 +0300

    draw charts with chartkick

--- Gemfile
+++ Gemfile
@@ -4,10 +4,11 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
-
+# Data representation library
+gem "chartkick"
 # style using material design
 gem 'materialize-sass'
-gem 'material_icons' 
+gem 'material_icons'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 # Use postgresql as the database for Active Record
--- app/controllers/data_collection_controller.rb
+++ app/controllers/data_collection_controller.rb
@@ -4,8 +4,19 @@ class DataCollectionController < ApplicationController
 
     end
 
-    def dashboard
+    def internet_user
+      render json: InternetUser.group(:year).sum(:hits)
+    end
 
+    def dashboard
+      @internet_data = InternetUser.group(:year).sum(:hits)
+      @mobile_data = MobileUser.group(:year).sum(:hits)
+      @facebook_data = FacebookUser.group(:year).sum(:hits)
+      @twitter_data = TwitterUser.group(:year).sum(:hits)
+      @instagram_data = InstagramUser.group(:year).sum(:hits)
+      @whatsapp_data = WhatsappUser.group(:year).sum(:hits)
+      @linkedin_data = LinkedinUser.group(:year).sum(:hits)
+      @telegram_data = TelegramUser.group(:year).sum(:hits)
     end
 
 end
